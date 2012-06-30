5893615d7ad4c5e97abeaaad636de1eb6d275eed
Jesse Atkinson <jesse.s.atkinson@gmail.com>
2012-06-30 14:49:04 -0400

    Many of Jason's changes.

--- js/script.js
+++ js/script.js
@@ -27,11 +27,23 @@
 		init: function () {
 			// this.resize();
 			// this.size();
+			this.video();
 			this.sticky();
 			this.scroller();
 			this.newsletter();
 			this.supporters();
 			this.responsive();
+		},
+
+		video: function () {
+			$(document).click({namespace: this}, function (e) {
+				var t = e.data.namespace;
+
+				if ($(e.target).parents("#video-slider-wrapper iframe").length > 0 || $(e.target).is($("#video-slider-wrapper iframe"))) {
+					console.log("hi");
+					$("#episode h1").hide();
+				}
+			});
 		},
 
 		resize: function () {
