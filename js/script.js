6b1834849c77ea7280c2433800c3d89da8a6e552
Jesse Atkinson <jesse.s.atkinson@gmail.com>
2012-07-22 10:59:49 -0400

    Converted all tabs to spaces for better cross text-editor and cross-IDE compatability. I recently found out that hard tabs cause issues in VI and other editors sometimes. Also, I \*finally\* figured out what was causing the right margin on the iPhone.

--- js/script.js
+++ js/script.js
@@ -10,196 +10,196 @@
 // <a href="mailto:jesse.s.atkinson@gmail.com">email me</a>!</em>
 
 (function ($, $f, window, undefined) {
-	"use strict";
+    "use strict";
 
-	var app = {
-		init: function () {
-			this.newsletter();
-			this.position();
-			this.resize();
-			this.scroller();
-			this.sticky();
-			this.supporters();
-			this.video();
-		},
+    var app = {
+        init: function () {
+            this.newsletter();
+            this.position();
+            this.resize();
+            this.scroller();
+            this.sticky();
+            this.supporters();
+            this.video();
+        },
 
-		newsletter: function () {
-			var subscribe = $(".subscribe_header").html();
+        newsletter: function () {
+            var subscribe = $(".subscribe_header").html();
 
-			$(document).click({namespace: this}, function (e) {
-				var t = e.data.namespace, newsletter, wrapper, footer;
+            $(document).click({namespace: this}, function (e) {
+                var t = e.data.namespace, newsletter, wrapper, footer;
 
-				if ($(e.target).is($(".newsletter"))) {
-					e.preventDefault();
+                if ($(e.target).is($(".newsletter"))) {
+                    e.preventDefault();
 
-					if ($(e.target).is($($(".newsletter")[0]))) {
-						newsletter = $($(".newsletter")[0]);
-						wrapper = $(".subscribe_header");
-						footer = false;
-						$(".subscribe_footer").html("");
-					} else {
-						newsletter = $($(".newsletter")[1]);
-						wrapper = $(".subscribe_footer");
-						footer = true;
-						$(".subscribe_header").html("");
-					}
+                    if ($(e.target).is($($(".newsletter")[0]))) {
+                        newsletter = $($(".newsletter")[0]);
+                        wrapper = $(".subscribe_header");
+                        footer = false;
+                        $(".subscribe_footer").html("");
+                    } else {
+                        newsletter = $($(".newsletter")[1]);
+                        wrapper = $(".subscribe_footer");
+                        footer = true;
+                        $(".subscribe_header").html("");
+                    }
 
-					newsletter.toggleClass("active");
-					wrapper.html(subscribe).toggle();
+                    newsletter.toggleClass("active");
+                    wrapper.html(subscribe).toggle();
 
-					if (footer) {
-						$("#subscribe").addClass("footer");
-					}
-				} else if ($(e.target).parents("#subscribe").length > 0) {
-					if ($(e.target).is($("#submit"))) {
-						$(".newsletter").removeClass("active");
-						$(".subscribe_header, .subscribe_footer").hide();
-					}
-				} else {
-					$(".newsletter").removeClass("active");
-					$(".subscribe_header, .subscribe_footer").hide();
-				}
-			});
-		},
+                    if (footer) {
+                        $("#subscribe").addClass("footer");
+                    }
+                } else if ($(e.target).parents("#subscribe").length > 0) {
+                    if ($(e.target).is($("#submit"))) {
+                        $(".newsletter").removeClass("active");
+                        $(".subscribe_header, .subscribe_footer").hide();
+                    }
+                } else {
+                    $(".newsletter").removeClass("active");
+                    $(".subscribe_header, .subscribe_footer").hide();
+                }
+            });
+        },
 
-		resize: function () {
-			$(window).resize({namespace: this}, function (e) {
-				var t = e.data.namespace;
+        resize: function () {
+            $(window).resize({namespace: this}, function (e) {
+                var t = e.data.namespace;
 
-				// Re-grabs the value on resize because the distance from the
-				// top of the browser to the top nav is different.
-				t.position();
-			});
-		},
+                // Re-grabs the value on resize because the distance from the
+                // top of the browser to the top nav is different.
+                t.position();
+            });
+        },
 
-		position: function () {
-			var marginLeft = $("header h1").width() / 2;
+        position: function () {
+            var marginLeft = $("header h1").width() / 2;
 
-			// Ensures that the header text stays centered and also only shows
-			// after all the fonts have loaded.
-			$("header h1").css("margin-left", "-" + marginLeft + "px").show();
-		},
+            // Ensures that the header text stays centered and also only shows
+            // after all the fonts have loaded.
+            $("header h1").css("margin-left", "-" + marginLeft + "px").show();
+        },
 
-		scroller: function () {
-			$(".top-nav .links").localScroll();
-			$(".top-nav .nav").click(function () {
-				$(".top-nav .nav").removeClass("active");
-				$(this).addClass("active");
-			});
+        scroller: function () {
+            $(".top-nav .links").localScroll();
+            $(".top-nav .nav").click(function () {
+                $(".top-nav .nav").removeClass("active");
+                $(this).addClass("active");
+            });
 
-			$($("footer .nav")[1]).click(function (e) {
-				e.preventDefault();
+            $($("footer .nav")[1]).click(function (e) {
+                e.preventDefault();
 
-				$(this).toggleClass("active");
-			});
-		},
+                $(this).toggleClass("active");
+            });
+        },
 
-		sticky: function () {
-			var t = this;
+        sticky: function () {
+            var t = this;
 
-			$(window).scroll({namespace: this}, function (e) {
-				var t = e.data.namespace,
-					aboveHeight = $("header").outerHeight();
+            $(window).scroll({namespace: this}, function (e) {
+                var t = e.data.namespace,
+                    aboveHeight = $("header").outerHeight();
 
-				if ($(window).scrollTop() > aboveHeight) {
-					$(".top-nav").addClass("fixed");
-					$("body").addClass("fixed");
-				} else {
-					$(".top-nav").removeClass("fixed");
-					$("body").removeClass("fixed");
-				}
+                if ($(window).scrollTop() > aboveHeight) {
+                    $(".top-nav").addClass("fixed");
+                    $("body").addClass("fixed");
+                } else {
+                    $(".top-nav").removeClass("fixed");
+                    $("body").removeClass("fixed");
+                }
 
-				// This handles the passing of the .active class around as we scroll
-				// up and down the page.
-				$.each($(".top-nav .nav"), function (i, v) {
-					var href = $(this).attr("href");
+                // This handles the passing of the .active class around as we scroll
+                // up and down the page.
+                $.each($(".top-nav .nav"), function (i, v) {
+                    var href = $(this).attr("href");
 
-					if (href.indexOf("#") !== -1) {
-						if ($(window).scrollTop() >= $(href).offset().top) {
-							$(".top-nav .nav").removeClass("active");
-							$(this).addClass("active");
+                    if (href.indexOf("#") !== -1) {
+                        if ($(window).scrollTop() >= $(href).offset().top) {
+                            $(".top-nav .nav").removeClass("active");
+                            $(this).addClass("active");
 
-							if ($(window).scrollTop() === ($(document).height() - $(window).height())) {
-								$(".top-nav .nav").removeClass("active");
-								$($(".top-nav .nav")[4]).addClass("active");
-							}
-						}
-					}
-				});
-			});
-		},
+                            if ($(window).scrollTop() === ($(document).height() - $(window).height())) {
+                                $(".top-nav .nav").removeClass("active");
+                                $($(".top-nav .nav")[4]).addClass("active");
+                            }
+                        }
+                    }
+                });
+            });
+        },
 
-		supporters: function () {
-			var auto;
+        supporters: function () {
+            var auto;
 
-			$("#supporters_button").click(function (e) {
-				e.preventDefault();
+            $("#supporters_button").click(function (e) {
+                e.preventDefault();
 
-				$("#supporters").css("height", "auto");
-				auto = $("#supporters").height();
-				$("#supporters").css("height", "");
+                $("#supporters").css("height", "auto");
+                auto = $("#supporters").height();
+                $("#supporters").css("height", "");
 
-				$(this).toggleClass("active");
+                $(this).toggleClass("active");
 
-				if ($(this).hasClass("active")) {
-					$(this).removeClass("active");
+                if ($(this).hasClass("active")) {
+                    $(this).removeClass("active");
 
-					$("#supporters").animate({
-						height: "0"
-					}, 500).css("border-top-width", "0");
-				} else {
-					$(this).addClass("active");
+                    $("#supporters").animate({
+                        height: "0"
+                    }, 500).css("border-top-width", "0");
+                } else {
+                    $(this).addClass("active");
 
-					$("#supporters").animate({
-						height: auto
-					}, {
-						duration: 100,
-						complete: function () {
-							$("html, body").animate({
-								scrollTop: $("footer").offset().top
-							}, 500);
-						}
-					}).css("border-top-width", "3px");
-				}
-			});
+                    $("#supporters").animate({
+                        height: auto
+                    }, {
+                        duration: 100,
+                        complete: function () {
+                            $("html, body").animate({
+                                scrollTop: $("footer").offset().top
+                            }, 500);
+                        }
+                    }).css("border-top-width", "3px");
+                }
+            });
 
-			$("#supporters button").click(function (e) {
-				e.preventDefault();
+            $("#supporters button").click(function (e) {
+                e.preventDefault();
 
-				$(this).removeClass("active");
-				$("#supporters").animate({
-					height: "",
-					borderTopColor: "",
-					borderTopStyle: "",
-					borderTopWidth: ""
-				}, 500);
-			});
-		},
+                $(this).removeClass("active");
+                $("#supporters").animate({
+                    height: "",
+                    borderTopColor: "",
+                    borderTopStyle: "",
+                    borderTopWidth: ""
+                }, 500);
+            });
+        },
 
-		video: function () {
-			var iframe = $("#video")[0],
-				player = $f(iframe),
-				onPlay, onPause, onFinish;
+        video: function () {
+            var iframe = $("#video")[0],
+                player = $f(iframe),
+                onPlay, onPause, onFinish;
 
-			player.addEvent('ready', function() {
-				player.addEvent('play', onPlay);
-				player.addEvent('pause', onPause);
-				player.addEvent('finish', onPause);
-			});
+            player.addEvent('ready', function() {
+                player.addEvent('play', onPlay);
+                player.addEvent('pause', onPause);
+                player.addEvent('finish', onPause);
+            });
 
-			onPlay = function () {
-				$("#episode h1").hide();
-			};
+            onPlay = function () {
+                $("#episode h1").hide();
+            };
 
-			onPause = function () {
-				$("#episode h1").show();
-			};
-		}
-	};
+            onPause = function () {
+                $("#episode h1").show();
+            };
+        }
+    };
 
-	$(document).ready(function () {
-		app.init();
-	});
+    $(document).ready(function () {
+        app.init();
+    });
 
-	window.app = app || {};
+    window.app = app || {};
 }(jQuery, $f, this));
