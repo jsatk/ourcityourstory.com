3697a797a6b51b55a21dd35acbc6fa06f8a725e4
Jesse Atkinson <jesse.s.atkinson@gmail.com>
2012-07-29 15:50:24 -0400

    TESTING...

--- allstories/index.php
+++ allstories/index.php
@@ -17,24 +17,7 @@
     <body class="allstories">
 
         <?php include ($_SERVER['DOCUMENT_ROOT'] . "/dev/includes/facebook.php"); ?>
-
-        <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
-                   chromium.org/developers/how-tos/chrome-frame-getting-started -->
-                <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-
-        <header>
-            <img class="bg rel" src="img/videostories-bg.jpg" alt="Header" width="1752px" height="984px">
-            <!-- For iPhone 4 only -->
-            <img class="iphone rel" src="img/header-bg-m.jpg" alt="Header" width="320px" height="240px">
-            <h1 class="title">Our City, Our Story aims to find and tell the stories which make up our identity. This is Rockford, Illinois.</h1>
-
-            <a href="http://ourcityourstory.com" class="logo sprite"><span class="hidden">Our City, Our Story</span></a>
-
-            <a href="" class="latest cushycms">
-                <label>Latest Story</label>
-                <em>Episode 11: Buddy Baseball</em>
-            </a>
-        </header>
+        <?php include ($_SERVER['DOCUMENT_ROOT'] . "/dev/includes/header.php"); ?>
 
         <div class="top-nav">
             <nav>
