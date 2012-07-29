beeea58c54ac8285149bd831a7492176c900bf66
Jesse Atkinson <jesse.s.atkinson@gmail.com>
2012-07-29 15:48:55 -0400

    TESTING...

--- allstories/index.php
+++ allstories/index.php
@@ -36,7 +36,46 @@
             </a>
         </header>
 
-        <?php include ($_SERVER['DOCUMENT_ROOT'] . "/dev/includes/top-nav.php"); ?>
+        <div class="top-nav">
+            <nav>
+            <!--
+                A note on the use of non-breaking spaces to create the nav. This was the best, simplest, and cleanest way to achieve what was comped. What was comped was using true | characters within the Proxima Nova font rather than a CSS border. To keep the | character of the Proxima Nova font and not break from style I had to use &nbsp; to force four spaces before and after each | character.
+            -->
+                <div class="links">
+                    <a href="/#episode" class="nav scroll">Episode</a><span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span>
+                    <a href="/#about" class="nav scroll">About</a><span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span>
+                    <a href="/#recent" class="nav scroll">Recent</a><span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span>
+                    <a href="/#contact" class="nav scroll">Contact</a><span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span>
+                    <a href="#sponsor" class="cut nav scroll">Sponsor</a><span class="cut">&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span>
+                    <a href="http://ourcityourstory.bigcartel.com/" target="_blank"  class="cut nav">Shop</a><span class="cut">&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span>
+                    <a href="/dev/allstories/" class="nav">All<span> Stories</span></a>
+                    <a href="mailto:pablokorona@ourcityourstory.com" class="social email sprite newsletter"><span class="hidden">Email</span></a>
+                    <a href="https://twitter.com/ourcityourstory" class="social twitter sprite" target="_blank"><span class="hidden">Twitter</span></a>
+                    <a href="https://www.facebook.com/OurCityOurStory" class="social facebook sprite" target="_blank"><span class="hidden">Facebook</span></a>
+                </div>
+
+                <div class="subscribe_header clearfix">
+                    <div id="subscribe" class="subscribe">
+                        <!-- Begin MailChimp Signup Form -->
+                        <div id="mc_embed_signup">
+                            <form action="http://OurCityOurStory.us4.list-manage2.com/subscribe/post?u=c9520922a151606a6cd234c19&amp;id=b51c4e7078" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                                <label>Subscribe to Newsletter</label>
+                                <div class="mc-field-group">
+                                    <label for="mce-EMAIL" style="display: none;">Email Address </label>
+                                    <input type="email" value="" name="EMAIL" class="required" id="mce-EMAIL" placeholder="Enter Your Email Address">
+                                </div>
+                                <div id="mce-responses" class="clear">
+                                    <div class="response" id="mce-error-response" style="display: none"></div>
+                                    <div class="response" id="mce-success-response" style="display: none"></div>
+                                </div>
+                                <input id="submit" type="submit" value="SIGN UP" name="subscribe" id="mc-embedded-subscribe" class="button">
+                            </form>
+                        </div>
+                        <!--End mc_embed_signup-->
+                    </div>
+                </div>
+            </nav>
+        </div>
 
         <div role="main">
             <div id="recent" class="recent clearfix">
