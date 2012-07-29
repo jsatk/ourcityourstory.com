c1f38bd13a6851f0a830ddd3891930ee640a6a2e
Jesse Atkinson <jesse.s.atkinson@gmail.com>
2012-07-29 15:24:15 -0400

    Initial push of All Stories page.

---
+++ allstories.php
@@ -0,0 +1,109 @@
+<!doctype html>
+    <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+    <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+    <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+    <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+    <!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+    <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+    <head>
+        <?php include ($_SERVER['DOCUMENT_ROOT'] . "/dev/includes/head.php"); ?>
+
+        <meta class="cushycms" title="Meta-Description" name="description" content="Rockford, IL, Storytelling video project, Stories of Rockford, Story video project, Rockford hometown pride building, Rockford illinois">
+        <meta class="cushycms" title="Facebook Share Title" property="og:title" content="Our City, Our Story">
+        <meta class="cushycms" title="Facebook Share Image." property="og:image" content="http://ourcityourstory.com/img/logo.png">
+    </head>
+
+    <body class="allstories">
+
+        <?php include ($_SERVER['DOCUMENT_ROOT'] . "/dev/includes/facebook.php"); ?>
+
+        <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
+                   chromium.org/developers/how-tos/chrome-frame-getting-started -->
+                <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+
+        <header>
+            <img class="bg rel" src="img/videostories-bg.jpg" alt="Header" width="1752px" height="984px">
+            <!-- For iPhone 4 only -->
+            <img class="iphone rel" src="img/header-bg-m.jpg" alt="Header" width="320px" height="240px">
+            <h1 class="title">Our City, Our Story aims to find and tell the stories which make up our identity. This is Rockford, Illinois.</h1>
+
+            <a href="http://ourcityourstory.com" class="logo sprite"><span class="hidden">Our City, Our Story</span></a>
+
+            <a href="" class="latest cushycms">
+                <label>Latest Story</label>
+                <em>Episode 11: Buddy Baseball</em>
+            </a>
+        </header>
+
+        <?php include ($_SERVER['DOCUMENT_ROOT'] . "/dev/includes/top-nav.php"); ?>
+
+        <div role="main">
+            <div id="recent" class="recent clearfix">
+                <article class="clearfix">
+                    <h5>Our City, Our Story</h5>
+                    <h1>Recent Stories</h1>
+
+                    <div title="Episode #1" class="column left cushycms">
+                        <a href="http://ourcityourstory.com/episode/10">
+                            <img class="replace-2x rel" src="img/e10-thumb.jpg" alt="E10: Fabric and Color Patterns" />
+                        </a>
+                        <h6>Episode 10</h6>
+                        <h3><a href="http://ourcityourstory.com/episode/10">Fabric &amp; Color Patterns</a></h3>
+                        <p>The flag is more than just fabric and color patterns. Its a reminder&hellip; a point of reference, it gives us a way collectively to think about how far we&#39;ve come and yet how far we have to go. People gave their lives so that we could live in a better country&hellip;</p>
+                        <a href="http://ourcityourstory.com/episode/10" class="button" target="_blank">Watch &rarr;</a>
+                    </div>
+
+                    <div title="Episode #2" class="column center cushycms"><a href="http://ourcityourstory.com/episode/9"><img class="replace-2x rel" src="img/e09-thumb.jpg" alt="E09: Hazzard Free" /></a>
+                        <h6>Episode 09</h6>
+                        <h3><a href="http://ourcityourstory.com/episode/9">Hazzard Free</a></h3>
+                        <p>Andy Hazzard had been a staple of downtown Rockford before she decided to acknowledge her true calling of farming. She was a waitress at Octane, she worked on jewelry at several art fairs, but when it came down to it, she had far too strong&hellip;</p>
+                        <a href="http://ourcityourstory.com/episode/9" class="button" target="_blank">Watch &rarr;</a>
+                    </div>
+
+                    <div title="Episode #3" class="column right cushycms"><a href="http://ourcityourstory.com/episode/8">
+                            <img class="replace-2x rel" src="img/e08-thumb.jpg" alt="recent1" />
+                        </a>
+                        <h6>Episode 08</h6>
+                        <h3><a href="http://ourcityourstory.com/episode/8">Team Fur Bandit</a></h3>
+                        <p>&ldquo;Did I just really see that?&rdquo; It was the middle of December, and on a snow soaked Sunday afternoon a couple dozen fur covered bikes were sprinting down East State Street. Directly after this mass of bikes was a white truck&hellip;</p>
+                        <a href="http://ourcityourstory.com/episode/8" class="button">Watch &rarr;</a>
+                    </div>
+
+                    <div title="Episode #4" class="column left cushycms">
+                        <a href="http://ourcityourstory.com/episode/10">
+                            <img class="replace-2x rel" src="img/e10-thumb.jpg" alt="E10: Fabric and Color Patterns" />
+                        </a>
+                        <h6>Episode 10</h6>
+                        <h3><a href="http://ourcityourstory.com/episode/10">Fabric &amp; Color Patterns</a></h3>
+                        <p>The flag is more than just fabric and color patterns. Its a reminder&hellip; a point of reference, it gives us a way collectively to think about how far we&#39;ve come and yet how far we have to go. People gave their lives so that we could live in a better country&hellip;</p>
+                        <a href="http://ourcityourstory.com/episode/10" class="button" target="_blank">Watch &rarr;</a>
+                    </div>
+
+                    <div title="Episode #5" class="column center cushycms"><a href="http://ourcityourstory.com/episode/9"><img class="replace-2x rel" src="img/e09-thumb.jpg" alt="E09: Hazzard Free" /></a>
+                        <h6>Episode 09</h6>
+                        <h3><a href="http://ourcityourstory.com/episode/9">Hazzard Free</a></h3>
+                        <p>Andy Hazzard had been a staple of downtown Rockford before she decided to acknowledge her true calling of farming. She was a waitress at Octane, she worked on jewelry at several art fairs, but when it came down to it, she had far too strong&hellip;</p>
+                        <a href="http://ourcityourstory.com/episode/9" class="button" target="_blank">Watch &rarr;</a>
+                    </div>
+
+                    <div title="Episode #6" class="column right cushycms"><a href="http://ourcityourstory.com/episode/8">
+                            <img class="replace-2x rel" src="img/e08-thumb.jpg" alt="recent1" />
+                        </a>
+                        <h6>Episode 08</h6>
+                        <h3><a href="http://ourcityourstory.com/episode/8">Team Fur Bandit</a></h3>
+                        <p>&ldquo;Did I just really see that?&rdquo; It was the middle of December, and on a snow soaked Sunday afternoon a couple dozen fur covered bikes were sprinting down East State Street. Directly after this mass of bikes was a white truck&hellip;</p>
+                        <a href="http://ourcityourstory.com/episode/8" class="button">Watch &rarr;</a>
+                    </div>
+
+                </article>
+            </div>
+        </div>
+        <!-- End of role="main" -->
+
+        <?php include ($_SERVER['DOCUMENT_ROOT'] . "/dev/includes/sponsor.php"); ?>
+        <?php include ($_SERVER['DOCUMENT_ROOT'] . "/dev/includes/supporters.php"); ?>
+        <?php include ($_SERVER['DOCUMENT_ROOT'] . "/dev/includes/footer.php"); ?>
+
+    </body>
+</html>
