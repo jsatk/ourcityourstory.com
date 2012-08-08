fcc38d62a49fa455f606bc1f9f0e586c7aa523f4
Jesse Atkinson <jesse.s.atkinson@gmail.com>
2012-08-08 16:04:19 -0400

    Fixes for Facebook.

--- episode/12/index.php
+++ episode/12/index.php
@@ -9,7 +9,8 @@
     <head>
         <?php include ($_SERVER['DOCUMENT_ROOT'] . "/includes/head.php"); ?>
 
-        <meta name="description" property="og:description" class="cushycms" title="Meta-Description" content="Rockford, IL, Storytelling video project, Stories of Rockford, Story video project, Rockford hometown pride building, Rockford illinois">        <meta property="og:title" class="cushycms" title="Facebook Share Title" content="Our City, Our Story | MainfraiM" />
+        <meta name="description" property="og:description" class="cushycms" title="Meta-Description" content="Rockford, IL, Storytelling video project, Stories of Rockford, Story video project, Rockford hometown pride building, Rockford illinois">
+        <meta property="og:title" class="cushycms" title="Facebook Share Title" content="Our City, Our Story | MainfraiM" />
         <meta property="og:image" class="cushycms" title="Facebook Share Image" content="http://ourcityourstory.com/img/f12.jpg" />
         <meta property="og:type" class="cushycms" content="company" />
         <meta property="og:url" class="cushycms" content="http://ourcityourstory.com/episode/12/" />
