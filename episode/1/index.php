6cfff97a7bb66753b3d85f6fb75321b285a5be5f
Pablo Korona <pablokorona@gmail.com>
2012-08-11 16:19:18 -0700

    Pushing Pablo's changes.

--- episode/1/index.php
+++ episode/1/index.php
@@ -6,26 +6,25 @@
     <!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
     <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 
-    <head class="cushycms" title="Meta Tags">
-        <meta charset="utf-8">
+    <head title="Meta Tags" class="cushycms">        <meta charset="utf-8" />
 
         <!-- Use the .htaccess and remove these lines to avoid edge case issues.
            More info: h5bp.com/i/378 -->
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
 
         <title>Storytelling video product to help define the identity of this community. This is Rockford, Illinois.</title>
 
         <!-- Mobile viewport optimized: h5bp.com/viewport. Width was originally set to 'device-width' but it was breaking. -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <!-- Base URL. Change/disable/re-enable depending on environment. -->
         <!-- <base href="http://localhost:8888/"> -->
         <!-- <base href="http://ourcityourstory.com/dev/"> -->
-        <base href="http://ourcityourstory.com/">
+        <base href="http://ourcityourstory.com/" />
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
 
-        <link class="rel" rel="stylesheet" href="css/style.css">
+        <link href="css/style.css" class="rel" rel="stylesheet" />
 
         <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
 
@@ -35,13 +34,13 @@
         <script class="rel" src="js/libs/modernizr-2.5.3.min.js"></script>
 
 
-        <meta name="description" property="og:description" title="Meta-Description" content="1955. Vincent Chiarelli of Sicily arrived to Rockford, IL with a song in his heart and a needle in his pocket. He opened a small tailor shop and spun out a music publishing company with his own record label from its success. His shop was located at the head of Broadway Street coming downtown, and those who came to see Vince thought he owned the town.">
+        <meta name="description" property="og:description" title="Meta-Description" content="1955. Vincent Chiarelli of Sicily arrived to Rockford, IL with a song in his heart and a needle in his pocket. He opened a small tailor shop and spun out a music publishing company with his own record label from its success. His shop was located at the head of Broadway Street coming downtown, and those who came to see Vince thought he owned the town." />
         <meta property="og:title" title="Facebook Share Title" content="Our City, Our Story | The Torchlighter" />
         <meta property="og:image" title="Facebook Share Image" content="http://ourcityourstory.com/img/f01.jpg" />
-        <meta property="og:type"content="movie" />
+        <meta property="og:type" content="movie" />
         <meta property="og:url" content="http://ourcityourstory.com/episode/1/" />
         <meta property="og:site_name" content="Our City, Our Story" />
-        <meta property="fb:admins" content="631463218">
+        <meta property="fb:admins" content="631463218" />
     </head>
 
     <body>
@@ -52,20 +51,20 @@
 
     <div role="main">
         <div id="episode" class="episode">
-            <h1 title="Featured video title" class="cushycms">The Torchlighter</h1>
+            <h1 class="cushycms" title="Featured video title">The Torchlighter</h1>
             <!-- I found this awesome trick here. http://www.netmagazine.com/tutorials/create-fluid-width-videos -->
 
-            <h1 class="hidden cushycms" title="Is this the most recent episode?" id="current">No</h1>
+            <h1 title="Is this the most recent episode?" class="hidden cushycms" id="current">No</h1>
 
-            <a href="#" class="controls next"><em>&#8592;</em><span title="Next Episode" class="cushycms">E02: Jimmy Goodman</span></a>
-            <a href="#" class="controls previous"><span title="Previous Episode" class="cushycms"></span><em>&#8594;</em></a>
-            <div title="Change the number after 'video/' but before '?'." class="cushycms"><iframe title="iframe for featured episode." mozallowfullscreen="" src="http://player.vimeo.com/video/43273534?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;api=1&amp;player_id=video" allowfullscreen="" id="video" height="281" webkitallowfullscreen="" width="500"></iframe></div>
+            <a href="#" class="controls next"><em>&#8592;</em><span class="cushycms" title="Next Episode">E02: Jimmy Goodman</span></a>
+            <a href="#" class="controls previous"><span class="cushycms" title="Previous Episode"></span><em>&#8594;</em></a>
+            <div class="cushycms" title="Change the number after 'video/' but before '?'."><iframe title="iframe for featured episode." mozallowfullscreen="" src="http://player.vimeo.com/video/43273534?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;api=1&amp;player_id=video" allowfullscreen="" id="video" height="281" webkitallowfullscreen="" width="500"></iframe></div>
         </div>
 
         <div class="story clearfix">
             <article class="clearfix">
                 <h5>Our City, Our Story</h5>
-                <h4 title="Featured episode title. Must begin with 'E##'" class="cushycms">E01: The Torchlighter</h4>
+                <h4 class="cushycms" title="Featured episode title. Must begin with 'E##'">E01: The Torchlighter</h4>
                 <div class="column left">
                     <img class="thumb replace-2x rel" src="" alt="Featured Episode Thumbnail">
                     <nav class="clearfix">
@@ -83,7 +82,7 @@
                     </nav>
 
                 </div>
-                <div title="Feature episode description" class="column center clearfix cushycms"><p>
+                <div class="column center clearfix cushycms" title="Feature episode description"><p>
 	1955. Vincent Chiarelli of Sicily arrived to Rockford, IL with a song in his heart and a needle in his pocket. He opened a small tailor shop and spun out a music publishing company with his own record label from its success. His shop was located at the head of Broadway Street coming downtown, and those who came to see Vince thought he owned the town.</p>
 <p>
 	Passion for music is the lifeblood of the Chiarelli family, and most of this information was passed along not by the sons of Vince the Tailor, but by his grandson of the same name, Little Vincent Chiarelli. Vince the Singer started up Rockford College Radio, which laid dormant for 20 years. He has taken the torch of passion for music and has helped others get their voices heard.</p>
@@ -93,15 +92,11 @@
 	When the last interview question, &ldquo;Is there anything else you want to say?&rdquo;, arose...Vince, now over 80 years of age, said - &ldquo;I&rsquo;m thankful for you doing this...I don&rsquo;t know how much longer I&rsquo;m going to be here.&rdquo; To our eyes, he is an important figure in Rockford history and we are grateful to have come upon this first flicker of light that has illuminated the way of Our City, Our Story.</p>
 <a href="#" class="button next">&larr; Next<span class="cut">&nbsp;Episode</span></a> <a href="#" class="button previous">Previous<span class="cut">&nbsp;Episode</span> &rarr;</a> </div>
 
-                <div title="Featured episode credits" class="column right cushycms"><ul>
+                <div class="column right cushycms" title="Featured episode credits"><ul>
 	<li>
 		<h6>
 			Directed &amp; Produced By</h6>
 		<span>Pablo Korona</span></li>
-	<li>
-		<h6>
-			Assistant &amp; Second Shooter</h6>
-		<span>Julia Moore</span></li>
 	<li>
 		<h6>
 			Music</h6>
@@ -113,8 +108,8 @@
 	<li>
 		<h6>
 			Special Thanks</h6>
-		<span>TBD</span><br />
-		<br />
+		Rockford College</li>
+	<li>
 		&nbsp;</li>
 </ul>
 </div>
@@ -127,7 +122,7 @@
             <article class="clearfix">
                 <h5>Our City, Our Story</h5>
                 <h1>Recent Stories</h1>
-                <div title="Recent episode #1" class="column left cushycms"><a href="http://ourcityourstory.com/episode/4#episode"><img class="replace-2x rel" src="img/e04-thumb.jpg" alt="E04: Darrell Hines" /></a>
+                <div class="column left cushycms" title="Recent episode #1"><a href="http://ourcityourstory.com/episode/4#episode"><img class="replace-2x rel" src="img/e04-thumb.jpg" alt="E04: Darrell Hines" /></a>
 <h6>
 	Episode 04</h6>
 <h3>
@@ -135,7 +130,7 @@
 <p>
 	Epictetus said, &ldquo;He is a wise man who does not grieve for the things which he has not, but rejoices for those which he has.&rdquo; We live in a world amply sprinkled with naysayers. Hearing the word NO more than YES has become&hellip;</p>
 <a href="http://ourcityourstory.com/episode/4#episode" class="button">Watch &rarr;</a></div>
-                <div title="Recent episode #2" class="column center cushycms"><a href="http://ourcityourstory.com/episode/3/#episode"><img class="replace-2x rel" src="img/e03-thumb.jpg" alt="E03: Pioneer of the Past" /></a>
+                <div class="column center cushycms" title="Recent episode #2"><a href="http://ourcityourstory.com/episode/3/#episode"><img class="replace-2x rel" src="img/e03-thumb.jpg" alt="E03: Pioneer of the Past" /></a>
 <h6>
 	Episode 03</h6>
 <h3>
@@ -143,7 +138,7 @@
 <p>
 	Can one man change the world? Yes&mdash;pre-eminent historian James Henry Breasted did just that, simply by changing the way we look at the world and its history. Elementary school students first learn&hellip;</p>
 <a href="http://ourcityourstory.com/episode/3/#episode" class="button">Watch &rarr;</a></div>
-                <div title="Recent episode #3" class="column right cushycms"><a href="http://ourcityourstory.com/episode/2#episode"><img class="replace-2x rel" src="img/e02-thumb.jpg" alt="E02: Jimmy Goodman" /></a>
+                <div class="column right cushycms" title="Recent episode #3"><a href="http://ourcityourstory.com/episode/2#episode"><img class="replace-2x rel" src="img/e02-thumb.jpg" alt="E02: Jimmy Goodman" /></a>
 <h6>
 	Episode 02</h6>
 <h3>
