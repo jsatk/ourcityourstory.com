35a255c602e6e19cc7ec36bf437eb21df56a74bb
Pablo Korona <pablokorona@gmail.com>
2013-03-17 00:00:02 -0700

    Cron job

--- includes/supporters.php
+++ includes/supporters.php
@@ -5,180 +5,206 @@
             <h2>Supporters</h2>
             <span>The following growing list of people have made Our City, Our Story possible.</span>
         </div>
-        <ul title="Supporters" class="cushycms clearfix">
-            <ul>
-                <li class="column">
-                    <ul>
-                        <li><h6>Producer</h6></li>
-                        <li>Pablo Korona</li>
-                        <li><h6 class="padding">Editorial/<br />Advisory Board</h6></li>
-                        <li>Linda Niemiec</li>
-                        <li>Courtney Oertel</li>
-                        <li>Shelton Kaye</li>
-                        <li>Chris Wachowiak</li>
-                        <li>Shawn Kelley</li>
-                        <li>Ron Clewer</li>
-                        <li>Jim Barbagallo</li>
-                        <li><h6 class="padding">Associate Producers ($100+)</h6></li>
-                        <li>Jojo G.</li>
-                        <li>Anthony Adamany</li>
-                        <li>Wendy Young</li>
-                        <li>Cyndie Hall</li>
-                        <li>Andrew Carlson</li>
-                        <li>Chris Manuel</li>
-                        <li>Eirik Augusston</li>
-                        <li>Deric Bloomingdale</li>
-                        <li>Janyce Fadden</li>
-                        <li>Katie Dorland</li>
-                        <li>Tony Chiarelli</li>
-                        <li>Sarah Adler</li>
-                        <li>Courtney Oertel</li>
-                    </ul>
-                </li>
-                <li class="column margin">
-                    <ul>
-                        <li>Kari McDonald - The Paper Button Studios</li>
-                        <li>Bret Konsdorf</li>
-                        <li>Jacob Rush</li>
-                        <li>Barton Bishoff</li>
-                        <li>Annie O&#39;Keefe</li>
-                        <li>Belles Firm of Architecture, Inc</li>
-                        <li>Victoria Melton</li>
-                        <li>John Groh</li>
-                        <li>Matthew Jeppsen</li>
-                        <li>Betsy Youngquist</li>
-                        <li>Cultivate Studios</li>
-                        <li>Mindy Young</li>
-                        <li>Woodfire/Pasta Cucina</li>
-                        <li>Arnold Dyer</li>
-                        <li>MedicineMan</li>
-                        <li>Joe Kelly</li>
-                        <li>Denise Guzzardo</li>
-                        <li><h6 class="padding">Sponsors ($25+)</h6></li>
-                        <li>Katrina McGrath</li>
-                        <li>Katie Martin</li>
-                        <li>Guillermo Martinez</li>
-                        <li>Paul Octavious</li>
-                        <li>Jay Schaul</li>
-                        <li>Jason Ryan</li>
-                        <li>Andrew Kitzmiller</li>
-                        <li>Eric Borchardt</li>
-                    </ul>
-                </li>
-                <li class="column margin">
-                    <ul>
-                        <li>Linda Grist Cunningham</li>
-                        <li>Patrick McDonough</li>
-                        <li>Joelene Chinn</li>
-                        <li>Bernice Armould</li>
-                        <li>Tom Leu</li>
-                        <li>Anna Brahmstedt Akerlund</li>
-                        <li>Nels Akerlund</li>
-                        <li>Jane Austin</li>
-                        <li>Irene Korona Dyer</li>
-                        <li>Gregory Farnham</li>
-                        <li>Ryan Davis</li>
-                        <li>Branson Werner</li>
-                        <li>MacFanBoy</li>
-                        <li>Andy Whorehall</li>
-                        <li>Tom &amp; Sarah McNamara</li>
-                        <li>Roger Peterson Jr.</li>
-                        <li>Rachael Pennell</li>
-                        <li>Meghan Jones Turley</li>
-                        <li>Ashley Ray-Harris</li>
-                        <li>Worland Guitars</li>
-                        <li>Lana M. Paris</li>
-                        <li>Andrew H Davis</li>
-                        <li>Eric Kelley</li>
-                        <li>Katie Barisa</li>
-                        <li>Jordan Guzzardo</li>
-                        <li>Mary H. Jones</li>
-                        <li>Linda Niemiec</li>
-                        <li>Jenica Rogers</li>
-                    </ul>
-                </li>
-                <li class="column margin">
-                    <ul>
-                        <li>Brian L. Shelton</li>
-                        <li>Gary &amp; Maddy Walters</li>
-                        <li>Jason Hoss</li>
-                        <li>Culture Shock Clothing and Records</li>
-                        <li>Mike Tarrolly</li>
-                        <li>Charlie Schweinler</li>
-                        <li>Max Gersh</li>
-                        <li>Mr. &amp; Mrs. Kirk Wescom</li>
-                        <li>Carol Bingley</li>
-                        <li>Mya Sadler</li>
-                        <li>Shayna Leber</li>
-                        <li>Alicia DiBenedetto Neubauer</li>
-                        <li>Amber Kresol</li>
-                        <li>Joe Hammack</li>
-                        <li>Government Spy</li>
-                        <li>Becky Martin</li>
-                        <li>Michael Lindvall</li>
-                        <li>Gins Ross</li>
-                        <li>Rebecca Shaffer</li>
-                        <li>Susan Fumo</li>
-                        <li>Joe Frisella</li>
-                        <li>Danny Lorden</li>
-                        <li>Steven Hall</li>
-                        <li>Jen Shelton</li>
-                    </ul>
-                </li>
-                <li class="column margin">
-                    <ul>
-                        <li><h6>Dear<br />#9Sponsors ($9+)</h6></li>
-                        <li>Kathy McNeely-Johnson</li>
-                        <li>Tricia Diduch</li>
-                        <li>Michael Kearney</li>
-                        <li>Sue Baily</li>
-                        <li>Michael D. Hopkins</li>
-                        <li>Aaron T.</li>
-                        <li>James D Madara</li>
-                        <li>Jason Judd</li>
-                        <li>Zak Rotello</li>
-                        <li>Jecca Green</li>
-                        <li>Scott Wilson</li>
-                        <li>Alya Adamany</li>
-                        <li>Callie Ezell</li>
-                        <li>Jason Ommodt</li>
-                        <li>Sarah Donohue</li>
-                        <li>Shawn Petersen</li>
-                        <li>Doug &amp; Holli Connell</li>
-                        <li>Javier Jimenez</li>
-                        <li>Kurt Laudicina</li>
-                        <li>John Curtis</li>
-                        <li>Planet5D</li>
-                        <li>Eva Frazier</li>
-                        <li>Lauran Cort Cacciatori</li>
-                        <li>Eli Nicolosi</li>
-                        <li>Kristina A.</li>
-                        <li>Elaine Breck</li>
-                        <li>Debra Werner</li>
-                    </ul>
-                </li>
-                <li class="column">
-                    <ul>
-                        <li>David Conroy</li>
-                        <li>Petrea Witt</li>
-                        <li>Pirate Ninja Print Shop</li>
-                        <li>Benjamin Grigg</li>
-                        <li>Deborah DeCastris</li>
-                        <li>Andrew Georgis</li>
-                        <li>Anne Duquennois</li>
-                        <li>Chris Cantwell</li>
-                        <li>Stefanie Dyer</li>
-                        <li>Kimberly Nelson</li>
-                        <li>Dominick Squicciarini</li>
-                        <li>Britney Dunnavan</li>
-                        <li>Sheri Meyers Cook</li>
-                        <li>Alicia Dyer</li>
-                        <li>Jessie Crow Mermel</li>
-                        <li>Julia Speck</li>
-                        <li>Patrick Delehanty</li>
-                    </ul>
-                </li>
-            </ul>
-        </ul>
+        <ul title="Supporters" class="cushycms clearfix"><ul>
+	<li class="column">
+	<ul>
+		<li>
+		<h6>Producer</h6>
+		</li>
+		<li>Pablo Korona</li>
+		<li>
+		<h6 class="padding">Editorial/<br />
+		Advisory Board</h6>
+		</li>
+		<li>Linda Niemiec</li>
+		<li>Courtney Oertel</li>
+		<li>Shelton Kaye</li>
+		<li>Chris Wachowiak</li>
+		<li>Shawn Kelley</li>
+		<li>Ron Clewer</li>
+		<li>Jim Barbagallo</li>
+		<li>
+		<h6 class="padding">Associate Producers ($100+)</h6>
+		</li>
+		<li>Juanita Hickerson</li>
+		<li>Bea Zammuto Miller</li>
+		<li>Marge &amp; Kyle Bevers</li>
+		<li>Mona Lindvall</li>
+		<li>Amy Belmont</li>
+		<li>Anthony Adamany</li>
+		<li>Wendy Young</li>
+		<li>Chuck Prorok &amp; Marcia Mueller</li>
+		<li>Gary Anderson</li>
+		<li>Kevin Holdman</li>
+		<li>Cyndie Hall</li>
+		<li>Dr. John &amp; Marge Maynard</li>
+		<li>Frank and Jody Perrecone</li>
+		<li>Dr. Joe &amp; Barb Pittman</li>
+		<li>Andrew Carlson</li>
+		<li>Chris Manuel</li>
+		<li>Anne Demsey</li>
+		<li>Eirik Augusston</li>
+		<li>Deric Bloomingdale</li>
+		<li>Janyce Fadden</li>
+		<li>Katie Dorland</li>
+		<li>Mr. Joel Ross</li>
+		<li>Tony Chiarelli</li>
+		<li>Sarah Adler</li>
+		<li>Courtney Oertel</li>
+	</ul>
+	</li>
+	<li class="column margin">
+	<ul>
+		<li>Kari McDonald - The Paper Button Studios</li>
+		<li>Bret Konsdorf</li>
+		<li>Jacob Rush</li>
+		<li>Betsy Makris</li>
+		<li>Barton Bishoff</li>
+		<li>Annie O&#39;Keefe</li>
+		<li>Belles Firm of Architecture, Inc</li>
+		<li>Victoria Melton</li>
+		<li>John Groh</li>
+		<li>Matthew Jeppsen</li>
+		<li>Betsy Youngquist</li>
+		<li>Cultivate Studios</li>
+		<li>Mindy Young</li>
+		<li>Jojo G.</li>
+		<li>Woodfire/Pasta Cucina</li>
+		<li>Arnold Dyer</li>
+		<li>MedicineMan</li>
+		<li>Joe Kelly</li>
+		<li>Denise Guzzardo</li>
+		<li>
+		<h6 class="padding">Sponsors ($25+)</h6>
+		</li>
+		<li>Gary Carlson</li>
+		<li>Katrina McGrath</li>
+		<li>Katie Martin</li>
+		<li>Guillermo Martinez</li>
+		<li>Paul Octavious</li>
+		<li>Jay Schaul</li>
+		<li>Jason Ryan</li>
+		<li>Andrew Kitzmiller</li>
+		<li>Eric Borchardt</li>
+	</ul>
+	</li>
+	<li class="column margin">
+	<ul>
+		<li>Linda Grist Cunningham</li>
+		<li>Patrick McDonough</li>
+		<li>Joelene Chinn</li>
+		<li>Bernice Armould</li>
+		<li>Tom Leu</li>
+		<li>Anna Brahmstedt Akerlund</li>
+		<li>Nels Akerlund</li>
+		<li>Jane Austin</li>
+		<li>Irene Korona Dyer</li>
+		<li>Gregory Farnham</li>
+		<li>Ryan Davis</li>
+		<li>Branson Werner</li>
+		<li>MacFanBoy</li>
+		<li>Andy Whorehall</li>
+		<li>Tom &amp; Sarah McNamara</li>
+		<li>Roger Peterson Jr.</li>
+		<li>Rachael Pennell</li>
+		<li>Meghan Jones Turley</li>
+		<li>Ashley Ray-Harris</li>
+		<li>Worland Guitars</li>
+		<li>Lana M. Paris</li>
+		<li>Andrew H Davis</li>
+		<li>Eric Kelley</li>
+		<li>Katie Barisa</li>
+		<li>Jordan Guzzardo</li>
+		<li>Mary H. Jones</li>
+		<li>Linda Niemiec</li>
+		<li>Jenica Rogers</li>
+	</ul>
+	</li>
+	<li class="column margin">
+	<ul>
+		<li>Brian L. Shelton</li>
+		<li>Gary &amp; Maddy Walters</li>
+		<li>Jason Hoss</li>
+		<li>Culture Shock Clothing and Records</li>
+		<li>Mike Tarrolly</li>
+		<li>Charlie Schweinler</li>
+		<li>Max Gersh</li>
+		<li>Mr. &amp; Mrs. Kirk Wescom</li>
+		<li>Carol Bingley</li>
+		<li>Mya Sadler</li>
+		<li>Shayna Leber</li>
+		<li>Alicia DiBenedetto Neubauer</li>
+		<li>Amber Kresol</li>
+		<li>Joe Hammack</li>
+		<li>Government Spy</li>
+		<li>Becky Martin</li>
+		<li>Michael Lindvall</li>
+		<li>Gins Ross</li>
+		<li>Rebecca Shaffer</li>
+		<li>Susan Fumo</li>
+		<li>Joe Frisella</li>
+		<li>Danny Lorden</li>
+		<li>Steven Hall</li>
+		<li>Jen Shelton</li>
+	</ul>
+	</li>
+	<li class="column margin">
+	<ul>
+		<li>
+		<h6>Dear<br />
+		#9Sponsors ($9+)</h6>
+		</li>
+		<li>Kathy McNeely-Johnson</li>
+		<li>Tricia Diduch</li>
+		<li>Michael Kearney</li>
+		<li>Sue Baily</li>
+		<li>Michael D. Hopkins</li>
+		<li>Aaron T.</li>
+		<li>James D Madara</li>
+		<li>Jason Judd</li>
+		<li>Zak Rotello</li>
+		<li>Jecca Green</li>
+		<li>Scott Wilson</li>
+		<li>Alya Adamany</li>
+		<li>Callie Ezell</li>
+		<li>Jason Ommodt</li>
+		<li>Sarah Donohue</li>
+		<li>Shawn Petersen</li>
+		<li>Doug &amp; Holli Connell</li>
+		<li>Javier Jimenez</li>
+		<li>Kurt Laudicina</li>
+		<li>John Curtis</li>
+		<li>Planet5D</li>
+		<li>Eva Frazier</li>
+		<li>Lauran Cort Cacciatori</li>
+		<li>Eli Nicolosi</li>
+		<li>Kristina A.</li>
+		<li>Elaine Breck</li>
+		<li>Debra Werner</li>
+	</ul>
+	</li>
+	<li class="column">
+	<ul>
+		<li>David Conroy</li>
+		<li>Petrea Witt</li>
+		<li>Pirate Ninja Print Shop</li>
+		<li>Benjamin Grigg</li>
+		<li>Deborah DeCastris</li>
+		<li>Andrew Georgis</li>
+		<li>Anne Duquennois</li>
+		<li>Chris Cantwell</li>
+		<li>Stefanie Dyer</li>
+		<li>Kimberly Nelson</li>
+		<li>Dominick Squicciarini</li>
+		<li>Britney Dunnavan</li>
+		<li>Sheri Meyers Cook</li>
+		<li>Alicia Dyer</li>
+		<li>Jessie Crow Mermel</li>
+		<li>Julia Speck</li>
+		<li>Patrick Delehanty</li>
+	</ul>
+	</li>
+</ul>
+</ul>
     </article>
 </div>
