459b4523c412288590f0ea118fd9b3c0fa7edadc
Pablo Korona <pablokorona@gmail.com>
2013-04-02 00:00:02 -0700

    Cron job

--- episode/1/index.php
+++ episode/1/index.php
@@ -7,7 +7,6 @@
     <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 
     <head class="cushycms" title="Meta Tags">        <meta charset="utf-8" />
-
         <!-- Use the .htaccess and remove these lines to avoid edge case issues.
            More info: h5bp.com/i/378 -->
         <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
@@ -40,7 +39,7 @@
         <meta property="og:type" content="movie" />
         <meta property="og:url" content="http://ourcityourstory.com/episode/1/" />
         <meta property="og:site_name" content="Our City, Our Story" />
-        <meta property="fb:app_id" content="268699596565206">
+        <meta property="fb:app_id" content="268699596565206" />
     </head>
 
     <body>
@@ -58,7 +57,7 @@
 
             <a href="#" class="controls next"><em>&#8592;</em><span title="Next Episode" class="cushycms">E02: Jimmy Goodman</span></a>
             <a href="#" class="controls previous"><span title="Previous Episode" class="cushycms"></span><em>&#8594;</em></a>
-            <div title="Change the number after 'video/' but before '?'." class="cushycms"><iframe title="iframe for featured episode." mozallowfullscreen="" src="http://player.vimeo.com/video/43273534?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;api=1&amp;player_id=video" allowfullscreen="" id="video" height="281" webkitallowfullscreen="" width="500"></iframe></div>
+            <div title="Change the number after 'video/' but before '?'." class="cushycms"><iframe allowfullscreen="" height="281" id="video" mozallowfullscreen="" src="http://player.vimeo.com/video/43273534?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;api=1&amp;player_id=video" title="iframe for featured episode." webkitallowfullscreen="" width="500"></iframe></div>
         </div>
 
         <div class="story clearfix">
@@ -82,35 +81,34 @@
                     </nav>
 
                 </div>
-                <div title="Feature episode description" class="column center clearfix cushycms"><p>
-	1955. Vincent Chiarelli of Sicily arrived to Rockford, IL with a song in his heart and a needle in his pocket. He opened a small tailor shop and spun out a music publishing company with his own record label from its success. His shop was located at the head of Broadway Street coming downtown, and those who came to see Vince thought he owned the town.</p>
-<p>
-	Passion for music is the lifeblood of the Chiarelli family, and most of this information was passed along not by the sons of Vince the Tailor, but by his grandson of the same name, Little Vincent Chiarelli. Vince the Singer started up Rockford College Radio, which laid dormant for 20 years. He has taken the torch of passion for music and has helped others get their voices heard.</p>
-<p>
-	By standards of the music industry, Vince Chiarelli never achieved the &ldquo;success&rdquo; of worldwide fame or became a household name like many dream of. What he has accomplished is something that the vast majority of them didn&rsquo;t. While most of these celebrities&rsquo; flame goes out, Vince the Singing Tailor lit many torches.</p>
-<p>
-	When the last interview question, &ldquo;Is there anything else you want to say?&rdquo;, arose...Vince, now over 80 years of age, said - &ldquo;I&rsquo;m thankful for you doing this...I don&rsquo;t know how much longer I&rsquo;m going to be here.&rdquo; To our eyes, he is an important figure in Rockford history and we are grateful to have come upon this first flicker of light that has illuminated the way of Our City, Our Story.</p>
-<a href="#" class="button next">&larr; Next<span class="cut">&nbsp;Episode</span></a> <a href="#" class="button previous">Previous<span class="cut">&nbsp;Episode</span> &rarr;</a> </div>
+                <div title="Feature episode description" class="column center clearfix cushycms"><p>1955. Vincent Chiarelli of Sicily arrived to Rockford, IL with a song in his heart and a needle in his pocket. He opened a small tailor shop and spun out a music publishing company with his own record label from its success. His shop was located at the head of Broadway Street coming downtown, and those who came to see Vince thought he owned the town.</p>
+
+<p>Passion for music is the lifeblood of the Chiarelli family, and most of this information was passed along not by the sons of Vince the Tailor, but by his grandson of the same name, Little Vincent Chiarelli. Vince the Singer started up Rockford College Radio, which laid dormant for 20 years. He has taken the torch of passion for music and has helped others get their voices heard.</p>
+
+<p>By standards of the music industry, Vince Chiarelli never achieved the &ldquo;success&rdquo; of worldwide fame or became a household name like many dream of. What he has accomplished is something that the vast majority of them didn&rsquo;t. While most of these celebrities&rsquo; flame goes out, Vince the Singing Tailor lit many torches.</p>
+
+<p>When the last interview question, &ldquo;Is there anything else you want to say?&rdquo;, arose...Vince, now over 80 years of age, said - &ldquo;I&rsquo;m thankful for you doing this...I don&rsquo;t know how much longer I&rsquo;m going to be here.&rdquo; To our eyes, he is an important figure in Rockford history and we are grateful to have come upon this first flicker of light that has illuminated the way of Our City, Our Story.<br />
+&nbsp;</p>
+
+<hr />
+<p>&nbsp;&nbsp;<br />
+In memory of Vince Chiarelli.</p>
+<a class="button next" href="#">&larr; Next<span class="cut">&nbsp;Episode</span></a> <a class="button previous" href="#">Previous<span class="cut">&nbsp;Episode</span> &rarr;</a></div>
 
                 <div title="Featured episode credits" class="column right cushycms"><ul>
 	<li>
-		<h6>
-			Directed &amp; Produced By</h6>
-		<span>Pablo Korona</span></li>
+	<h6>Directed &amp; Produced By</h6>
+	<span>Pablo Korona</span></li>
 	<li>
-		<h6>
-			Music</h6>
-		<span>Daniel James McMahon</span></li>
+	<h6>Music</h6>
+	<span>Daniel James McMahon</span></li>
 	<li>
-		<h6>
-			Audio</h6>
-		<span>Zach Staas</span></li>
+	<h6>Audio</h6>
+	<span>Zach Staas</span></li>
 	<li>
-		<h6>
-			Special Thanks</h6>
-		<span>Rockford College</span></li>
-	<li>
-		&nbsp;</li>
+	<h6>Special Thanks</h6>
+	<span>Rockford College</span></li>
+	<li>&nbsp;</li>
 </ul>
 </div>
             </article>
@@ -122,30 +120,27 @@
             <article class="clearfix">
                 <h5>Our City, Our Story</h5>
                 <h1>Recent Stories</h1>
-                <div title="Recent episode #1" class="column left cushycms"><a href="http://ourcityourstory.com/episode/4#episode"><img class="replace-2x rel" src="img/e04-thumb.jpg" alt="E04: Darrell Hines" /></a>
-<h6>
-	Episode 04</h6>
-<h3>
-	<a href="http://ourcityourstory.com/episode/4#episode">Darrell Hines</a></h3>
-<p>
-	Epictetus said, &ldquo;He is a wise man who does not grieve for the things which he has not, but rejoices for those which he has.&rdquo; We live in a world amply sprinkled with naysayers. Hearing the word NO more than YES has become&hellip;</p>
-<a href="http://ourcityourstory.com/episode/4#episode" class="button">Watch &rarr;</a></div>
-                <div title="Recent episode #2" class="column center cushycms"><a href="http://ourcityourstory.com/episode/3/#episode"><img class="replace-2x rel" src="img/e03-thumb.jpg" alt="E03: Pioneer of the Past" /></a>
-<h6>
-	Episode 03</h6>
-<h3>
-	<a href="http://ourcityourstory.com/episode/3/#episode">Pioneer of the Past</a></h3>
-<p>
-	Can one man change the world? Yes&mdash;pre-eminent historian James Henry Breasted did just that, simply by changing the way we look at the world and its history. Elementary school students first learn&hellip;</p>
-<a href="http://ourcityourstory.com/episode/3/#episode" class="button">Watch &rarr;</a></div>
-                <div title="Recent episode #3" class="column right cushycms"><a href="http://ourcityourstory.com/episode/2#episode"><img class="replace-2x rel" src="img/e02-thumb.jpg" alt="E02: Jimmy Goodman" /></a>
-<h6>
-	Episode 02</h6>
-<h3>
-	<a href="http://ourcityourstory.com/episode/2#episode">Jimmy Goodman</a></h3>
-<p>
-	Jimmy Goodman lives up to his surname. Three days a week, he volunteers in running and coaching youth in the sport of boxing. When you walk in to the space, you immediately see the ring before you, but you may not get to step in it for months&hellip;</p>
-<a href="http://ourcityourstory.com/episode/2#episode" class="button">Watch &rarr;</a></div>
+                <div title="Recent episode #1" class="column left cushycms"><a href="http://ourcityourstory.com/episode/4#episode"><img alt="E04: Darrell Hines" class="replace-2x rel" src="img/e04-thumb.jpg" /></a>
+<h6>Episode 04</h6>
+
+<h3><a href="http://ourcityourstory.com/episode/4#episode">Darrell Hines</a></h3>
+
+<p>Epictetus said, &ldquo;He is a wise man who does not grieve for the things which he has not, but rejoices for those which he has.&rdquo; We live in a world amply sprinkled with naysayers. Hearing the word NO more than YES has become&hellip;</p>
+<a class="button" href="http://ourcityourstory.com/episode/4#episode">Watch &rarr;</a></div>
+                <div title="Recent episode #2" class="column center cushycms"><a href="http://ourcityourstory.com/episode/3/#episode"><img alt="E03: Pioneer of the Past" class="replace-2x rel" src="img/e03-thumb.jpg" /></a>
+<h6>Episode 03</h6>
+
+<h3><a href="http://ourcityourstory.com/episode/3/#episode">Pioneer of the Past</a></h3>
+
+<p>Can one man change the world? Yes&mdash;pre-eminent historian James Henry Breasted did just that, simply by changing the way we look at the world and its history. Elementary school students first learn&hellip;</p>
+<a class="button" href="http://ourcityourstory.com/episode/3/#episode">Watch &rarr;</a></div>
+                <div title="Recent episode #3" class="column right cushycms"><a href="http://ourcityourstory.com/episode/2#episode"><img alt="E02: Jimmy Goodman" class="replace-2x rel" src="img/e02-thumb.jpg" /></a>
+<h6>Episode 02</h6>
+
+<h3><a href="http://ourcityourstory.com/episode/2#episode">Jimmy Goodman</a></h3>
+
+<p>Jimmy Goodman lives up to his surname. Three days a week, he volunteers in running and coaching youth in the sport of boxing. When you walk in to the space, you immediately see the ring before you, but you may not get to step in it for months&hellip;</p>
+<a class="button" href="http://ourcityourstory.com/episode/2#episode">Watch &rarr;</a></div>
                     <?php include ($_SERVER['DOCUMENT_ROOT'] . "/includes/allstorieslink.php"); ?>
             </article>
         </div>
