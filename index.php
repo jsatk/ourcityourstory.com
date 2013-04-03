08ea85ff3d0f9751e84f7bc4a9dff9b69a3d1302
Pablo Korona <pablokorona@gmail.com>
2013-04-03 00:00:03 -0700

    Cron job

--- index.php
+++ index.php
@@ -13,15 +13,18 @@
         <!-- Use the .htaccess and remove these lines to avoid edge case issues.
            More info: h5bp.com/i/378 -->
         <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
-        <title>Storytelling video project to help define the identity of this community. This is Rockford, Illinois.</title>
+        <title>Storytelling video product to help define the identity of this community. This is Rockford, Illinois.</title>
         <!-- Mobile viewport optimized: h5bp.com/viewport. Width was originally set to 'device-width' but it was breaking. -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <!-- Base URL. Change/disable/re-enable depending on environment. -->
         <!-- <base href="http://localhost:8888/"> -->
-        <!-- <base href="http://ourcityourstory.com/"> -->
+        <!-- <base href="http://ourcityourstory.com/dev/"> -->
         <base href="http://ourcityourstory.com/" />
+
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+
         <link href="css/style.css" class="rel" rel="stylesheet" />
+
         <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
 
         <!-- All JavaScript at the bottom, except this Modernizr build.
@@ -29,11 +32,12 @@
            Create your own custom Modernizr build: www.modernizr.com/download/ -->
         <script class="rel" src="js/libs/modernizr-2.5.3.min.js"></script>
 
-        <meta name="description" property="og:description" title="Meta-Description" content="It started with a tweet, and now Rockford, IL is partnering with ETSY to help get it back to the roots. Artisan. Hand made. Opportunity in the Making." />
-        <meta property="og:title" title="Facebook Share Title" content="Our City, Our Story | Opportunity in the Making" />
-        <meta property="og:image" title="Facebook Share Image" content="http://ourcityourstory.com/img/f15.jpg" />
-        <meta property="og:type" content="company" />
-        <meta property="og:url" content="http://ourcityourstory.com/episode/15/" />
+
+        <meta name="description" property="og:description" title="Meta-Description" content="1955. Vincent Chiarelli of Sicily arrived to Rockford, IL with a song in his heart and a needle in his pocket. He opened a small tailor shop and spun out a music publishing company with his own record label from its success. His shop was located at the head of Broadway Street coming downtown, and those who came to see Vince thought he owned the town." />
+        <meta property="og:title" title="Facebook Share Title" content="Our City, Our Story | The Torchlighter" />
+        <meta property="og:image" title="Facebook Share Image" content="http://ourcityourstory.com/img/f01.jpg" />
+        <meta property="og:type" content="movie" />
+        <meta property="og:url" content="http://ourcityourstory.com/episode/1/" />
         <meta property="og:site_name" content="Our City, Our Story" />
         <meta property="fb:app_id" content="268699596565206" />
     </head>
@@ -46,19 +50,19 @@
 
         <div role="main">
             <div id="episode" class="episode">
-                <h1 class="cushycms" title="Featured video title">Opportunity in the Making</h1>
+                <h1 class="cushycms" title="Featured video title">The Torchlighter</h1>
                 <!-- I found this awesome trick here. http://www.netmagazine.com/tutorials/create-fluid-width-videos -->
 
                 <h1 class="hidden cushycms" title="Is this the most recent episode?" id="current">Yes</h1>
-                <a href="" class="controls next"><em>&#8592;</em><span class="cushycms" title="Next Episode"></span></a>
-                <a href="" class="controls previous"><span class="cushycms" title="Previous Episode">E14: Potato Shak</span><em>&#8594;</em></a>
-                <div title="Change the number after 'video/' but before '?'." class="cushycms"><iframe allowfullscreen="" height="281" id="video" mozallowfullscreen="" src="http://player.vimeo.com/video/61305313?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;api=1&amp;player_id=video" title="iframe for featured episode." webkitallowfullscreen="" width="500"></iframe></div>
+                <a href="" class="controls next"><em>&#8592;</em><span class="cushycms" title="Next Episode">E02: Jimmy Goodman</span></a>
+                <a href="" class="controls previous"><span class="cushycms" title="Previous Episode">E15: Opportunity in the Making</span><em>&#8594;</em></a>
+                <div title="Change the number after 'video/' but before '?'." class="cushycms"><iframe allowfullscreen="" height="281" id="video" mozallowfullscreen="" src="http://player.vimeo.com/video/43273534?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;api=1&amp;player_id=video" title="iframe for featured episode." webkitallowfullscreen="" width="500"></iframe></div>
             </div>
 
             <div class="story clearfix">
                 <article class="clearfix">
                     <h5>Our City, Our Story</h5>
-                    <h4 class="cushycms" title="Featured episode title. Must begin with 'E##'">E15: Opportunity in the Making</h4>
+                    <h4 class="cushycms" title="Featured episode title. Must begin with 'E##'">E01: The Torchlighter</h4>
                     <div class="column left">
                     <img class="thumb replace-2x rel" src="" alt="Featured Episode Thumbnail">
                         <nav class="clearfix">
@@ -76,23 +80,18 @@
                         </nav>
                     </div>
 
-                    <div class="column clearfix center cushycms" title="Feature episode description"><p>It started with a tweet, and now Rockford, IL is partnering with ETSY to help get it back to the roots. Artisan. Hand made. Opportunity in the Making.</p>
+                    <div class="column clearfix center cushycms" title="Feature episode description"><p>1955. Vincent Chiarelli of Sicily arrived to Rockford, IL with a song in his heart and a needle in his pocket. He opened a small tailor shop and spun out a music publishing company with his own record label from its success. His shop was located at the head of Broadway Street coming downtown, and those who came to see Vince thought he owned the town.</p>
 
-<p>Story by&nbsp;<a href="https://www.etsy.com/blog/news/authors/beautifuld/">Dana Mauriello</a></p>
+<p>Passion for music is the lifeblood of the Chiarelli family, and most of this information was passed along not by the sons of Vince the Tailor, but by his grandson of the same name, Little Vincent Chiarelli. Vince the Singer started up Rockford College Radio, which laid dormant for 20 years. He has taken the torch of passion for music and has helped others get their voices heard.</p>
 
-<p>Last August, Mayor Larry Morrissey of Rockford, Illinois&nbsp;tweeted&nbsp;at Etsy&rsquo;s CEO, Chad Dickerson: &ldquo;Since we need an &lsquo;Etsy Economy&rsquo; has Etsy begun any partnerships with high schools or job training? We&rsquo;d love to explore.&rdquo; Kari McDonald, the captain of the&nbsp;Rockford Etsy Team, joined in on the dialogue: &ldquo;I&rsquo;d love to see where this leads! Rockford Etsy Team is willing to help.&rdquo; With these two tweets, we got to work.<br />
-<br />
-Rockford is a city of 150,000 people, located two hours west of Chicago. Formerly a manufacturing hub, its keystone employers have left the city, ushering in a wave of high unemployment. One of the benefits of having a strong manufacturing history, however, is that many residents already have skills in the arts (such as watchmaking and furniture making) that were once the backbone of the local economy. Mayor Morrissey is an enthusiastic advocate of giving Rockford residents the tools that they need to turn these skills into economic opportunity and we&rsquo;re eager to support this vision.<br />
-<br />
-A group of Etsy Admin went to Rockford in November to further explore the needs of the city and opportunities to work together. We were greeted by a diverse group of over 70 stakeholders who gathered to share their thoughts on how Etsy could affect local constituents.<br />
-<br />
-Our visit led to a plan for Etsy and Rockford to co-create a Craft Entrepreneurship Curriculum, with Etsy&rsquo;s platform and marketplace as the learning lab. The aim of the project is to teach people that if they have a craft skill, entrepreneurship and economic opportunity are within reach. Starting in September, the curriculum will be taught by local Rockford teachers to a diverse range of students.<br />
-<br />
-We don&rsquo;t expect every student to become an Etsy seller, but rather to apply the skills they learn to any entrepreneurial path they want to follow. We do believe, however, along with the city of Rockford, that this will lead to real economic impact.<br />
-<br />
-This pilot program has the potential to be not just what Mayor Morrissey calls a &ldquo;pathway to prosperity&rdquo; for Rockford, but a blueprint for similar programs across the country and around the world. We look to the Etsy community as role models for others dreaming of an entrepreneurial path and demonstrating the desire to support independent, creative businesses, built on&nbsp;human interactions. Thank you for inspiring us, the city of Rockford, and a wave of future entrepreneurs that are following in your footsteps.<br />
-<br />
-If you are interested in learning more about this project or talking to us about how Etsy can empower economic development in your area, please get in touch with us at <a href="mailto:partnerships@etsy.com">partnerships@etsy.com</a>. We want to hear about initiatives that we can apply our learnings to after the Rockford pilot program concludes.&nbsp;</p>
+<p>By standards of the music industry, Vince Chiarelli never achieved the &ldquo;success&rdquo; of worldwide fame or became a household name like many dream of. What he has accomplished is something that the vast majority of them didn&rsquo;t. While most of these celebrities&rsquo; flame goes out, Vince the Singing Tailor lit many torches.</p>
+
+<p>When the last interview question, &ldquo;Is there anything else you want to say?&rdquo;, arose...Vince, now over 80 years of age, said - &ldquo;I&rsquo;m thankful for you doing this...I don&rsquo;t know how much longer I&rsquo;m going to be here.&rdquo; To our eyes, he is an important figure in Rockford history and we are grateful to have come upon this first flicker of light that has illuminated the way of Our City, Our Story.<br />
+&nbsp;</p>
+
+<hr />
+<p>&nbsp;&nbsp;<br />
+In memory of Vince Chiarelli.</p>
 <a class="button next" href="#">&larr; Next<span class="cut">&nbsp;Episode</span></a> <a class="button previous" href="#">Previous<span class="cut">&nbsp;Episode</span> &rarr;</a></div>
 
                     <div class="column right cushycms" title="Featured episode credits"><ul>
@@ -107,16 +106,10 @@ If you are interested in learning more about this project or talking to us about
 	<span>Zach Staas</span></li>
 	<li>
 	<h6>Special Thanks</h6>
-	<span>Larry Morrissey<br />
-	Ron Clewer<br />
-	Juanita Whittier<br />
-	Chad Dickerson<br />
-	ETSY<br />
-	Seams2u</span><br />
-	<br />
-	&nbsp;</li>
-</ul>
-</div>
+	</li><li><span>Rockford College</span></li>
+	<li><span>Tony Chiarelli</span></li>
+	<li><span><a href="http://www.VincentRecords.com">Vincent Records</a></span></li>
+</ul></div>
                 </article>
             </div>
 
@@ -127,7 +120,15 @@ If you are interested in learning more about this project or talking to us about
                     <h5>Our City, Our Story</h5>
                     <h1>Recent Stories</h1>
 
-                    <div class="column left cushycms" title="Recent episode #1"><a href="http://ourcityourstory.com/episode/14/#episode"><img alt="E14: Potato Shak" class="replace-2x rel" src="img/e14-thumb.jpg" /></a>
+                    <div class="column left cushycms" title="Recent episode #1"><a href="http://ourcityourstory.com/episode/15/#episode"><img alt="E15: Opportunity in the Making" class="replace-2x rel" src="img/e15-thumb.jpg" /></a>
+<h6>Episode 15</h6>
+
+<h3><a href="http://ourcityourstory.com/episode/15/#episode">Opportunity in the Making</a></h3>
+
+<p>One tweet has potential to change the lives of the residents of our city.</p>
+<a class="button" href="http://ourcityourstory.com/episode/15/#episode" target="_blank">Watch &rarr;</a></div>
+
+                    <div class="column center cushycms" title="Recent episode #2"><a href="http://ourcityourstory.com/episode/14/#episode"><img alt="E14: Potato Shak" class="replace-2x rel" src="img/e14-thumb.jpg" /></a>
 <h6>Episode 14</h6>
 
 <h3><a href="http://ourcityourstory.com/episode/14/#episode">Potato Shak</a></h3>
@@ -135,21 +136,13 @@ If you are interested in learning more about this project or talking to us about
 <p>Peter Avramopoulos arrived in Rockford, Illinois in the late 1960s. His father, seeking prosperity, had brought his whole family from Greece by boat. Pete&rsquo;s prosperity came in the form of the opportunity to do well for himself and his immediate family.</p>
 <a class="button" href="http://ourcityourstory.com/episode/14/#episode" target="_blank">Watch &rarr;</a></div>
 
-                    <div class="column center cushycms" title="Recent episode #2"><a href="http://ourcityourstory.com/episode/13/#episode"><img alt="E13: Our Curiosity" class="replace-2x rel" src="img/e13-thumb.jpg" /></a>
+                    <div class="column right cushycms" title="Recent episode #3"><a href="http://ourcityourstory.com/episode/13/#episode"><img alt="E13: Our Curiosity" class="replace-2x rel" src="img/e13-thumb.jpg" /></a>
 <h6>Episode 13</h6>
 
 <h3><a href="http://ourcityourstory.com/episode/13/#episode">Our Curiosity</a></h3>
 
 <p>One of the most talked about scientific achievements in 2012, was the landing of the Mars rover, Curiosity. What brings this home for us, is ALL the gears for curiosity were made in this region. All of them, by these people.</p>
 <a class="button" href="http://ourcityourstory.com/episode/11/#episode" target="_blank">Watch &rarr;</a></div>
-
-                    <div class="column right cushycms" title="Recent episode #3"><a href="http://ourcityourstory.com/episode/12/#episode"><img alt="E12: MainfraiM" class="replace-2x rel" src="img/e12-thumb.jpg" /></a>
-<h6>Episode 12</h6>
-
-<h3><a href="http://ourcityourstory.com/episode/12/#episode">MainfraiM</a></h3>
-
-<p>Peer into the windows of their studio on Rockford&rsquo;s Central Avenue late one night, and chances are you&rsquo;ll find Jeremy Klonicki and Carmen Turner of MainfraiM Habitat for Art</p>
-<a class="button" href="http://ourcityourstory.com/episode/12/#episode" target="_blank">Watch &rarr;</a></div>
 
                     <?php include ($_SERVER['DOCUMENT_ROOT'] . "/includes/allstorieslink.php"); ?>
                 </article>
